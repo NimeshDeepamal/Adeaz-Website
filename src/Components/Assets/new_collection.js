c01c9df001d1ddc4be1a4aea8f8ce48b70591779
NimeshDeepamal <nimeshdeepamal@gmail.com>
2025-05-13 22:34:38 +0530

    Create new_collection.js

---
+++ src/Components/Assets/new_collection.js
@@ -0,0 +1,61 @@
+import p1_img from '../Assets/p1.png'
+import p2_img from '../Assets/p2.png'
+import p3_img from '../Assets/p3.png'
+import p4_img from '../Assets/p4.png'
+
+let data_product= [
+    {
+        id: 1,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p1_img, 
+        new_price: 50.00, 
+        old_price:80.50,
+    },
+    {
+        id: 2,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p2_img, 
+        new_price: 50.00, 
+        old_price:80.50,
+    },
+    {
+        id: 3,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p3_img, 
+        new_price: 50.00, 
+        old_price:80.50,
+    },{
+        id: 4,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p4_img, 
+        new_price: 50.00, 
+        old_price:80.50,
+    },
+    {
+        id: 3,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p3_img, 
+        new_price: 50.00, 
+        old_price:80.50,
+    },{
+        id: 4,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p4_img, 
+        new_price: 50.00, 
+        old_price:80.50,
+    },
+    {
+        id: 3,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p3_img, 
+        new_price: 50.00, 
+        old_price:80.50,
+    },{
+        id: 4,
+        name: "Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+        image: p4_img, 
+        new_price: 50.00, 
+        old_price:80.50,
+    }
+]
+export default data_product
