1b37b6164fec05d47b0280fc9387e317d060e196
NimeshDeepamal <nimeshdeepamal@gmail.com>
2025-05-13 22:36:05 +0530

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import './Navbar.css'
-import logo from '../Assets/logo.png'
+import logo from '../Assets/logo.jpg'
 import cart_item from '../Assets/cart_item.png'
 import profile_icon from '../Assets/profile.jpg'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
 
@@ -11,19 +12,19 @@ export default function Navbar() {
   return (
     <div className='navbar'>
       <div className="nav-logo">
-        <img src={logo} alt="logo" />
+      <Link to='/' style={{textDecoration: 'none'}}><img src={logo} alt="logo" /></Link>
       </div>
       <ul className="nav-menu">
-        <li onClick={()=> {setmenu("home")}}>Home{menu==="home"?<hr/>:<></>}</li>
-        <li onClick={()=> {setmenu("womens")}}>Women{menu==="women"?<hr/>:<></>}</li>
-        <li onClick={()=> {setmenu("mens")}}>Men{menu==="men"?<hr/>:<></>}</li>
-        <li onClick={()=> {setmenu("stores")}}>Store{menu==="store"?<hr/>:<></>}</li>
-        <li onClick={()=> {setmenu("about")}}>About{menu==="about"?<hr/>:<></>}</li>
-        <li onClick={()=> {setmenu("contact")}}>Contact{menu==="contact"?<hr/>:<></>}</li>
+        <li onClick={()=> {setmenu("home")}}><Link to='/' style={{textDecoration: 'none',color: 'white'}}>Home</Link>{menu==="home"?<hr/>:<></>}</li>
+        <li onClick={()=> {setmenu("womens")}}><Link to='/womens' style={{textDecoration: 'none',color: 'white'}}>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=> {setmenu("mens")}}><Link to='/mens' style={{textDecoration: 'none',color: 'white'}}>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=> {setmenu("store")}}><Link to='/store' style={{textDecoration: 'none',color: 'white'}}>Store</Link>{menu==="store"?<hr/>:<></>}</li>
+        <li onClick={()=> {setmenu("about")}}><Link to='/about' style={{textDecoration: 'none',color: 'white'}}>About</Link>{menu==="about"?<hr/>:<></>}</li>
+        <li onClick={()=> {setmenu("contact")}}><Link to='/contact' style={{textDecoration: 'none',color: 'white'}}>Contact</Link>{menu==="contact"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
-        <img src={profile_icon} alt="profile" />
-        <img src={cart_item} alt="cart" />
+      <Link to='/login'><img src={profile_icon} alt="profile" /></Link>
+      <Link to='/cart'><img src={cart_item} alt="cart" /></Link>
         <div className="nav-cart-count">0</div>
       </div>
     </div>
