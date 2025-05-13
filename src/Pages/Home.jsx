43161565fb03d38282e3a0f1e97515dd4dceab86
NimeshDeepamal <nimeshdeepamal@gmail.com>
2025-05-13 22:37:01 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Hero from '../Components/Hero/Hero.jsx'
+import Popular from '../Components/Popular/Popular.jsx'
+import Offers from '../Components/Offers/Offer.jsx'
+import NewCollection from '../Components/NewCollection/NewCollection.jsx'
+import NewsLetter from '../Components/NewsLetter/NewsLetter.jsx'
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home'>
+
+      <Hero/>
+      <Popular />
+      <Offers />
+      <NewCollection />
+      <NewsLetter />
     </div>
   )
 }
