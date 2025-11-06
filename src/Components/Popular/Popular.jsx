944ada1760b18ed29d194c61885c2c9a31a201ed
Nimesh Deepamal <nimeshdeepamal@gmail.com>
2025-11-06 16:08:32 +0530

    style: make responsive website

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -6,12 +6,19 @@ import Item from '../Items/Item'
 const Popular = () => {
   return (
     <div className='popular'>
-      <h1>Popular in women</h1>
+      <h1>Popular in Women</h1>
       <hr />
-      <div className="popular-item">
-        {data_product.map((item,i)=>{
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-        })}
+      <div className="popular-grid">
+        {data_product.map((item, i) => (
+          <Item
+            key={i}
+            id={item.id}
+            name={item.name}
+            image={item.image}
+            new_price={item.new_price}
+            old_price={item.old_price}
+          />
+        ))}
       </div>
     </div>
   )
